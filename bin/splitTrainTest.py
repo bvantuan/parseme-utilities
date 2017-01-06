14dbb88d85d17cc1a062a73fc4fdaa22395d8c9e
Carlos Ramisch <carlosramisch@gmail.com>
2017-01-06 10:51:23 +0100

    Rename gold -> test

--- bin/splitTrainTest.py
+++ bin/splitTrainTest.py
@@ -7,13 +7,13 @@ import subprocess
 import dataalign
 
 parser = argparse.ArgumentParser(description="""
-        Split input files into OUT/{train,gold}.{parsemetsv,conllu}.""")
+        Split input files into OUT/{train,test}.{parsemetsv,conllu}.""")
 parser.add_argument("--lang", choices=sorted(dataalign.LANGS), metavar="LANG", required=True,
         help="""Name of the target language (e.g. EN, FR, PL, DE...)""")
-parser.add_argument("--gold-first-sentence", type=int, default=1,
-        help="""Desired first sentence to use as blind & gold data (default: sentence 1)""")
-parser.add_argument("--gold-mwesize", type=int, default=500,
-        help="""Desired number of MWEs in blind & gold data""")
+parser.add_argument("--test-first-sentence", type=int, default=1,
+        help="""Desired first sentence to use as test data (default: sentence 1)""")
+parser.add_argument("--test-mwesize", type=int, default=500,
+        help="""Desired number of MWEs in test data""")
 parser.add_argument("--input", nargs="+", type=str, required=True,
         help="""Path to input files (in FoLiA XML or PARSEME TSV format)""")
 parser.add_argument("--conllu", nargs="+", type=str,
@@ -26,10 +26,10 @@ class Main:
         self.conllu_paths = self.args.conllu or dataalign.calculate_conllu_paths(self.args.input)
 
     def run(self):
-        info_train, info_gold = self.split_train_gold()
+        info_train, info_test = self.split_train_test()
         subprocess.check_call("mkdir -p ./OUT", shell=True)
         print("Statistics\n==========\n", file=sys.stderr)
-        for name, info in (("gold", info_gold), ("train", info_train)):
+        for name, info in (("test", info_test), ("train", info_train)):
             self.process_fileinfo(name, info)
 
     def process_fileinfo(self, name, info):
@@ -37,7 +37,7 @@ class Main:
         self.generate_file(name, "parsemetsv", info.tsv_lines)
         if self.conllu_paths:
             self.generate_file(name, "conllu", info.conllu_lines)
-        self.stats(info.mwecount, (name == "gold"))
+        self.stats(info.mwecount, (name == "test"))
 
     def generate_file(self, name, ext, lines):
         with open("./OUT/{}.{}".format(name, ext), "w+") as output:
@@ -45,8 +45,8 @@ class Main:
                 print(line, end="", file=output)
 
 
-    def stats(self, mwe_count, doing_gold):
-        print("### OUT/"+ ("gold.*" if doing_gold else "train.*"), file=sys.stderr)
+    def stats(self, mwe_count, doing_test):
+        print("### OUT/"+ ("test.*" if doing_test else "train.*"), file=sys.stderr)
         total = 0
         for mwetype,count in sorted(mwe_count.items()) :
             if mwetype != "TOTAL" :
@@ -54,15 +54,15 @@ class Main:
         print("  * **TOTAL**: {} VMWEs\n".format(mwe_count["TOTAL"]), file=sys.stderr)
 
 
-    def split_train_gold(self):
-        r"""Return a pair (train: FileInfo, gold: FileInfo)"""
-        info_train, info_gold = FileInfo(), FileInfo()
+    def split_train_test(self):
+        r"""Return a pair (train: FileInfo, test: FileInfo)"""
+        info_train, info_test = FileInfo(), FileInfo()
         info = info_train
 
         for sent_id, (tsv, conllu) in enumerate(self.iter_sentences(), 1):
-            if sent_id == self.args.gold_first_sentence:
-                info = info_gold
-            if info.mwecount["TOTAL"] >= self.args.gold_mwesize:
+            if sent_id == self.args.test_first_sentence:
+                info = info_test
+            if info.mwecount["TOTAL"] >= self.args.test_mwesize:
                 info = info_train
 
             info.tsv_lines.extend(tsv)
@@ -75,7 +75,7 @@ class Main:
                             mweid,mwetype = x.split(":")
                             info.mwecount[mwetype] = info.mwecount.get(mwetype,0) + 1
                             info.mwecount["TOTAL"] += 1
-        return info_train, info_gold
+        return info_train, info_test
 
 
     def iter_sentences(self):
