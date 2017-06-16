680378114d1ff8def73e7eafd6b895db8820f6b8
Carlos Ramisch <carlosramisch@gmail.com>
2017-06-16 15:11:33 +0200

    Update the repo structure so that we now have 2 versions of the guidelines

--- bin/toServer.sh
+++ bin/toServer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -rlptC --del --stats --progress -i ../parseme-st-guidelines/ carlos.ramisch@webequipe.lidil.univ-mrs.fr:/var/www/equipes/parsemefr/parseme-st-guidelines/1.1
+rsync -rlptC --del --stats --progress -i ../parseme-st-guidelines/1.0/ carlos.ramisch@webequipe.lidil.univ-mrs.fr:/var/www/equipes/parsemefr/guidelines-hypertext/
