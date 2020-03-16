b27bb70a37aaf63c7a2fd6c2056a14181595c309
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2020-03-16 10:52:56 +0100

    Splitting either ST1.2

---
+++ st-organizers/splitting/scripts/run_estimate_1.2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Params
+
+UNS_TEST=300
+UNS_DEV=100
+RND=50
+
+#Check the number of parameters
+if [ ! $# -eq 2 ]; then
+  echo Usage: `basename $0` 'ST-1.1-DIR' 'OUT-DIR'
+  echo
+  echo Run the estimation script for each language in PARSEME 1.1.
+  echo
+  exit
+fi
+
+OUT=$2
+
+# Echo
+set -x
+
+for lang_dir in $1/parseme*
+do
+  LANG="$(basename -- $lang_dir)"
+  mkdir $OUT/$LANG
+  ./split_cupt.py split -i $lang_dir/*.cupt -n $RND --unseen-test $UNS_TEST --unseen-dev $UNS_DEV --train-path $OUT/$LANG/train.cupt --dev-path $OUT/$LANG/dev.cupt --test-path $OUT/$LANG/test.cupt
+done
