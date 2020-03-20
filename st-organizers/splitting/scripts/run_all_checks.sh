d9a1f3e727f15955f19b1d08767280b3aed7910d
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2020-03-20 13:25:28 +0100

    Add script for running all checks

---
+++ st-organizers/splitting/scripts/run_all_checks.sh
@@ -0,0 +1,17 @@
+# Echo on
+set -x
+
+# Pull latest versions of all corpora
+apply-all-corpora.sh "git pull"
+
+# Check if the datasets are identical
+./scripts/check_all.sh scripts/langs/all.txt data/ST1.2 data/preliminary-sharedtask-data-alt
+
+# Move to directory with splits
+cd data/preliminary-sharedtask-data-alt
+
+# Inspect all validation logs
+cat */*valid*
+
+# Check for "sent_id"
+grep -l "# sent_id =" -R .
