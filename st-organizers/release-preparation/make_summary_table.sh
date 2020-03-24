60de026167c349269db4ec9f0dea4eca94498388
agata.savary@univ-tours.fr <agata.savary@univ-tours.fr>
2020-03-24 15:59:19 +0100

    Updating make_summary_table.sh with unsee stats

--- st-organizers/release-preparation/make_summary_table.sh
+++ st-organizers/release-preparation/make_summary_table.sh
@@ -16,7 +16,7 @@ fi
 
 
 for a in preliminary-sharedtask-data/??/; do
-#for a in preliminary-sharedtask-data-alt/??/; do
+#for a in preliminary-sharedtask-data-alt/??/; do	
   if [ -f $a/dev-stats.md ]; then
     cat $a/{train,dev,test}-stats.md
   elif [ -f $a/train-stats.md ]; then
@@ -28,7 +28,7 @@ sed -e 's@## File: [A-Z][A-Z]/@@g' -e 's/.cupt//g'\
     -e 's/Language: //g' -e 's/^\*.*: //g' |
 awk 'BEGIN{   prevlang = "XX";}
 /(test|train|dev)/{ # (train|test|dev) #<= CHANGEME IF NEEDED!  
-  head=$0 "  ";   
+  head=$2 "  ";   
   if(prevlang!=lang && prevlang != "XX"){    
     print rowsep;
     if (langsent!=0){
@@ -46,7 +46,7 @@ awk 'BEGIN{   prevlang = "XX";}
   getline; vmwe=$1;tvmwe+=vmwe; langvmwe+=vmwe;
   vid = 0; irv=0; lvcfull=0; vpcfull=0; vpcsemi=0; lvccause=0; iav=0; mvc=0; lsicv=0;
   do{
-    getline; 
+    getline; # print "line=", $0; 
     if($2 == "`VID`:"){    vid = $3; tvid += vid; langvid+= vid;   }
     else if($2 == "`IRV`:"){   irv = $3; tirv += irv;  langirv+= irv;   }
     else if($2 == "`LVC.full`:"){   lvcfull = $3; tlvcfull += lvcfull;   langlvcfull+= lvcfull;  }
@@ -55,25 +55,45 @@ awk 'BEGIN{   prevlang = "XX";}
     else if($2 == "`VPC.semi`:"){   vpcsemi = $3; tvpcsemi += vpcsemi;  langvpcsemi+= vpcsemi;   }            
     else if($2 == "`IAV`:"){   iav = $3; tiav += iav;  langiav+= iav;   }                
     else if($2 == "`MVC`:"){   mvc = $3; tmvc += mvc;  langmvc+= mvc;   }                
-    else if($2 == "`LS.ICV`:"){   lsicv = $3; tlsicv += lsicv;  langlsicv+= lsicv;   }                    
-  }while(NF > 1);  
+    else if($2 == "`LS.ICV`:"){   lsicv = $3; tlsicv += lsicv;  langlsicv+= lsicv;   } 
+  } while ($0 ~ /\*/);
+
+  #Adding the statistics of unseen
+  if (head ~ /(dev)|(test)/) {
+    unseen_wrt_train = $1;
+    getline
+    ratio_unseen_wrt_train = sprintf("%.2f", $1);
+  }
+  if (head ~ /test/) {
+    getline;  
+    unseen_wrt_train_dev = $1;
+    getline;  
+    ratio_unseen_wrt_train_dev = sprintf("%.2f", $1);
+  }
+
   if(prevlang != lang){
     #print rowsep;
   }
-  prevlang = lang;
+  prevlang = lang;	
 
-  print bline lang "-" head, sent, tok, int(10*(tok/sent))/10, vmwe, vid, irv, lvcfull, lvccause, vpcfull, vpcsemi, iav, mvc, lsicv eline;
+  if (head ~ /train/)
+    print bline lang "-" head, sent, tok, int(10*(tok/sent))/10, vmwe, vid, irv, lvcfull, lvccause, vpcfull, vpcsemi, iav, mvc, lsicv, "", "", "", "" eline;
+  else
+    if (head ~ /dev/)
+      print bline lang "-" head, sent, tok, int(10*(tok/sent))/10, vmwe, vid, irv, lvcfull, lvccause, vpcfull, vpcsemi, iav, mvc, lsicv, unseen_wrt_train, ratio_unseen_wrt_train, "", "" eline;
+    else
+      print bline lang "-" head, sent, tok, int(10*(tok/sent))/10, vmwe, vid, irv, lvcfull, lvccause, vpcfull, vpcsemi, iav, mvc, lsicv, unseen_wrt_train, ratio_unseen_wrt_train, unseen_wrt_train_dev, ratio_unseen_wrt_train_dev eline;
 }
-/^[A-Z][A-Z]$/{
-  lang=$0;  
+/Statistics [A-Z][A-Z]$/{
+  lang=$2;  
   if(NR==1){
     if(outformat == "html"){
       print "<table style=\"text-align:right\">\n<tbody>";          
       bline = "<tr><td style=\"font-weight:bold\">";
       OFS = "</td><td style=\"font-weight:bold\">";
       eline = "</td></tr>";
-      rowsep = "<tr style=\"height:1px; background-color: #d5d5d5;\"><td colspan=\"14\"></td></tr>";    
-      langsep = "<tr style=\"height:2px; background-color: #222222;\"><td colspan=\"14\"></td></tr>";          
+      rowsep = "<tr style=\"height:1px; background-color: #d5d5d5;\"><td colspan=\"18\"></td></tr>";    
+      langsep = "<tr style=\"height:2px; background-color: #222222;\"><td colspan=\"18\"></td></tr>";          
     }
     else if(outformat == "latex"){
       bline = "";      eline = " \\\\";    OFS = " & ";
@@ -88,7 +108,8 @@ awk 'BEGIN{   prevlang = "XX";}
 
     }    
     print langsep; 
-    print bline "Lang-split","Sentences","Tokens","Avg. length","VMWE", "VID", "IRV", "LVC.full", "LVC.cause", "VPC.full", "VPC.semi", "IAV", "MVC", "LS.ICV" eline;   
+#    print bline "Lang-split","Sentences","Tokens","Avg. length","VMWE", "VID", "IRV", "LVC.full", "LVC.cause", "VPC.full", "VPC.semi", "IAV", "MVC", "LS.ICV" eline;   
+    print bline "Lang-split","Sentences","Tokens","Avg. length","VMWE", "VID", "IRV", "LVC.full", "LVC.cause", "VPC.full", "VPC.semi", "IAV", "MVC", "LS.ICV", "Unseen wrt train", "Ratio unseen wrt train", "Unseen wrt train+dev", "Ratio unseen wrt train+dev", eline;   
     print langsep; 
     if(outformat == "html"){  
       bline = "<tr><td style=\"text-align:left\">";
