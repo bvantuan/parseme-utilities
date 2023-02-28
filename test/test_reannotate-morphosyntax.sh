353b8e5afb483bb7b09e55aac59b5869632b7259
BUI Van Tuan <buivantuan07@gmail.com>
2023-02-28 16:43:56 +0100

    modify morphosyntax test

--- test/test_reannotate-morphosyntax.sh
+++ test/test_reannotate-morphosyntax.sh
@@ -29,5 +29,5 @@ diff ./data_reannotation/parseme_test_eu.new.cupt ./data_reannotation/REANNOTATI
 run_devnull ../st-organizers/release-preparation/reannotate-morphosyntax.sh --method udtreebank -s ./data_reannotation/parseme_test_pl.cupt -t ./data_reannotation/ud-treebanks-v2.11/UD_Polish-PDB/ -u http://hdl.handle.net/11234/1-4923
 diff ./data_reannotation/parseme_test_pl.new.cupt ./data_reannotation/REANNOTATION/parseme_test_pl.new.cupt
 
-run_devnull ../st-organizers/release-preparation/reannotate-morphosyntax.sh --method udtreebank -s ./data_reannotation/parseme_test1_pl.cupt -t ./data_reannotation/parseme_test1_pl_pdb-ud.conllu -u http://hdl.handle.net/11234/1-4923 -p UD_Polish-PDB/pl_pdb-ud-dev.conllu
-diff ./data_reannotation/parseme_test1_pl.new.cupt ./data_reannotation/REANNOTATION/parseme_test1_pl.new.cupt
+# run_devnull ../st-organizers/release-preparation/reannotate-morphosyntax.sh --method udtreebank -s ./data_reannotation/parseme_test1_pl.cupt -t ./data_reannotation/parseme_test1_pl_pdb-ud.conllu -u http://hdl.handle.net/11234/1-4923 -p UD_Polish-PDB/pl_pdb-ud-dev.conllu
+# diff ./data_reannotation/parseme_test1_pl.new.cupt ./data_reannotation/REANNOTATION/parseme_test1_pl.new.cupt
